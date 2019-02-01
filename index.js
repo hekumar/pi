cf3c3ca1c0fb3e9c4efa552d63dfb88c037d2ee6
Hemant Kumar <hkumar06@in.ibm.com>
2019-02-01 20:13:12 +0530

    fulfillment error fixed 200 status

--- index.js
+++ index.js
@@ -5,19 +5,60 @@ var server = http.createServer(function(request, response) {
     // console.log(request.body.queryResult);
     response.writeHead(200, {"Content-Type": "application/json"});
     let responseObj ={
-        "fulfillmentText" : " ",
+        "fulfillmentText": "This is a text response",
         "fulfillmentMessages": [
-            {
-              "text": {
-                "text": [
-                    "Hello Hemant! How are you?"
-                ]
-              }
+          {
+            "card": {
+              "title": "card title",
+              "subtitle": "card text",
+             // "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+              "buttons": [
+                {
+                  "text": "button text",
+             //     "postback": "https://assistant.google.com/"
+                }
+              ]
             }
-          ],
-        "source" : ""
-
-    }
+          }
+        ],
+        "source": "example.com",
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": "this is a simple response"
+                  }
+                }
+              ]
+            }
+          },
+          "facebook": {
+            "text": "Hello, Facebook!"
+          },
+          "slack": {
+            "text": "This is a text response for Slack."
+          }
+        },
+        "outputContexts": [
+          {
+            "name": "pristinepi",
+            "lifespanCount": 5,
+            "parameters": {
+              "param": "param value"
+            }
+          }
+        ],
+        "followupEventInput": {
+          "name": "event name",
+          "languageCode": "en-US",
+          "parameters": {
+            "param": "param value"
+          }
+        }
+      };
     response.end(JSON.stringify(responseObj));
 
 });
