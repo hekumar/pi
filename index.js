a7211a67559bcf25419e375d6d027b03ee99d9d3
Hemant Kumar <hkumar06@in.ibm.com>
2019-02-01 19:50:09 +0530

    fulfillment response added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "application/json"});
     let responseObj ={
         "fulfillmentText" : " ",
-        "fulfillmentMessages" : [ { "test" : {"text" : ["Hello Hemant! How are you?"]}}]
+        "fulfillmentMessages" : [ { "test" : {"text" : ["Hello Hemant! How are you?"]}}],
+        "source" : ""
 
     }
     response.end(JSON.stringify(responseObj));
