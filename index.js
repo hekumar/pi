a6865d0fbd86cb23e93ad7d96ec2cad3fb27b201
Hemant Kumar <hkumar06@in.ibm.com>
2019-02-01 22:57:26 +0530

    every thing in place

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 var http = require('http');
-//const { WebhookClient } = require('dialogflow-fulfillment');
 
- //Create an instance
- //const agent = new WebhookClient({request: request, response: response});
 
 var server = http.createServer(function(request, response) {
 
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-   
+  
 
     response.writeHead(200, {"Content-Type": "application/json"});
     
    // response.end(JSON.stringify(responseObj));
-   response.end(JSON.stringify({
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "this is a simple response"
-              }
-            }
-          ]
-        }
-      }
-    }
-  })); 
+   response.end(JSON.stringify({"fulfillmentText": "hello how are you?"})); 
 
 });
 
